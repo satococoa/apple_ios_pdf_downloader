7c8c206cfe660efa91a02578ee86f1e3661e4e31
Satoshi Ebisawa <e.satoshi@gmail.com>
2013-05-04 22:20:24 +0900

    First working version

--- download.rb
+++ download.rb
@@ -1,6 +1,24 @@
 require 'bundler/setup'
+require 'pathname'
+require 'uri'
 Bundler.require
-PAGE_URI = 'https://developer.apple.com/jp/devcenter/ios/library/japanese.html'
 
-document = Nokogiri::HTML(HTTPClient.get(PAGE_URI).body)
-p document
+page_url = 'https://developer.apple.com/jp/devcenter/ios/library/japanese.html'
+page_uri = URI.parse(page_url)
+output_directory = './out'
+output_path = Pathname(output_directory)
+
+output_path.mkdir unless output_path.exist?
+document = Nokogiri::HTML(HTTPClient.get(page_url).body)
+
+document.search('a[href$="pdf"]').each do |link|
+  title = link.text
+  url = page_uri + link[:href]
+
+  puts "\e[32m===== downloading [#{title}](#{url}) =====\e[0m"
+
+  file = (output_path + title).to_s.gsub(/\s/, '') + '.pdf'
+  open(file, 'wb') {|f|
+    f.puts HTTPClient.get(url).body
+  }
+end
